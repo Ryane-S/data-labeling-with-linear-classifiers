eced4b15a5f303bdefc3d71cef759a43b5582dc4
Ry-Se-afk <sryane046@gmail.com>
2025-12-28 15:58:32 +0100

    Added KNN Classifier

--- src/nn_knn.py
+++ src/nn_knn.py
@@ -5,28 +5,17 @@ import keras
 import numpy as np
 
 
-def predictLabelNN(x_train:np.ndarray, y_train:np.ndarray, img:np.ndarray, distance_metric_L1_or_L2:bool) -> np.ndarray:
-    """Implement NN classifier."""
-    # Initialize the minimum score with infinity
-    scoreMin = float('inf')
+def most_frequent_one_hot(labels: list[np.ndarray]) -> np.ndarray:
+    """Return the most frequent one-hot vector in the list of one-hot vectors."""
+    # Convert list of arrays to 2D array
+    arr = np.stack(labels) 
+    
+    # Count occurrences of each unique row
+    unique_rows, counts = np.unique(arr, axis=0, return_counts=True)
+    
+    # Return the row with the highest count
+    return unique_rows[np.argmax(counts)]
 
-    # Loop over all training images to compute the distance with the test image
-    for idx, imgT in enumerate(x_train):
-        # Compute L1 (Manhattan) distance if specified
-        if distance_metric_L1_or_L2:
-            difference = np.abs(img-imgT)
-            score = np.sum(difference)
-        # Otherwise compute L2 (Euclidean) distance
-        else:
-            difference = np.square(img-imgT)
-            score = np.sqrt(np.sum(difference))
-
-        # Update the predicted label if this training image is closer than previous ones
-        if score < scoreMin :
-            scoreMin = score
-            predictedLabel = y_train[idx]
-
-    return predictedLabel
 
 def prepare_data(x_train:np.ndarray, y_train:np.ndarray, x_test:np.ndarray, y_test:np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Flatten and normalize the input datasets and convert labels to one-hot encoding."""
@@ -44,6 +33,64 @@ def prepare_data(x_train:np.ndarray, y_train:np.ndarray, x_test:np.ndarray, y_te
 
     return x_train, y_train, x_test, y_test
 
+
+def predictLabelNN(x_train:np.ndarray, y_train:np.ndarray, img:np.ndarray, distance_metric:bool) -> np.ndarray:
+    """Implement NN classifier."""
+    # Initialize the minimum score with infinity
+    scoreMin = float('inf')
+
+    # Loop over all training images to compute the distance with the test image
+    for idx, imgT in enumerate(x_train):
+        # Compute L1 (Manhattan) distance if specified
+        if distance_metric:
+            difference = np.abs(img-imgT)
+            score = np.sum(difference)
+        # Otherwise compute L2 (Euclidean) distance
+        else:
+            difference = np.square(img-imgT)
+            score = np.sqrt(np.sum(difference))
+
+        # Update the predicted label if this training image is closer than previous ones
+        if score < scoreMin :
+            scoreMin = score
+            predictedLabel = y_train[idx]
+
+    return predictedLabel
+
+
+def predictLabelKNN(x_train:np.ndarray, y_train:np.ndarray, img:np.ndarray, k:int, distance_metric:bool) -> np.ndarray:
+    """Implement KNN classifier."""
+    # Initialize a list to store the distances between the test image and each training image
+    predictions = []
+
+    # Loop over all training images to compute the distance with the test image
+    for idx, imgT in enumerate(x_train):
+        # Compute L1 (Manhattan) distance if specified and store it
+        if distance_metric:
+            difference = np.abs(img-imgT)
+            score = np.sum(difference)
+            predictions.append((score, y_train[idx]))
+        # Otherwise compute L2 (Euclidean) distance and store it
+        else:
+            difference = np.square(img-imgT)
+            score = np.sqrt(np.sum(difference))
+            predictions.append((score, y_train[idx]))
+
+    # Sort all elements in the predictions list in ascending order based on scores
+    predictions.sort(key=lambda item: item[0])
+   
+    # Retain only the top k predictions
+    predictions = predictions[:k]
+    
+    # Extract in a separate vector only the labels for the top k predictions
+    predictedLabels = [x[1] for x in predictions]
+
+    # Determine the dominant class from the predicted labels
+    predictedLabel = most_frequent_one_hot(predictedLabels)
+
+    return predictedLabel
+
+
 def main():
     """Implement KNN classification."""
     # Load the CIFAR-10 dataset
@@ -55,7 +102,7 @@ def main():
     # Predictions
     numberOfCorrectPredictedImages = 0
 
-    for idx, img in enumerate(x_test[0:200]):
+    for idx, img in enumerate(x_test):
         print(f'Make prediction for image {idx}')
 
         # Predict label using NN algorithm
